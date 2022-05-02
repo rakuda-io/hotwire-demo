b0774d337f9af047380c231ebb07e1b8c8c81165
YukiKubota <rakudalms@gmail.com>
2022-05-02 17:52:19 +0900

    Bootstrap5で見た目を整える

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -9,52 +9,44 @@ class CatsController < ApplicationController
     @cats = @search.result.page(params[:page]).per PAGE
   end
 
-  # GET /cats/1
   def show
   end
 
-  # GET /cats/new
   def new
     @cat = Cat.new
   end
 
-  # GET /cats/1/edit
   def edit
   end
 
-  # POST /cats
   def create
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "Cat was successfully created."
+      redirect_to @cat, notice: "ねこを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated."
+      redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /cats/1
   def destroy
     @cat.destroy
-    redirect_to cats_url, notice: "Cat was successfully destroyed."
+    redirect_to cats_url, notice: "ねこを削除しました。"
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_cat
       @cat = Cat.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def cat_params
       params.require(:cat).permit(:name, :age)
     end
